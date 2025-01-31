01f6daa587d30d7bcb962bd4b86fd7c2c17a3927
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-31 15:42:55 -0300

    Edx: CS50: update linked_list code

--- edx/cs50/week-5-data-structures/linked_list.c
+++ edx/cs50/week-5-data-structures/linked_list.c
@@ -1,3 +1,8 @@
+// #include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct node
 {
     char* phrase;
@@ -5,12 +10,63 @@ typedef struct node
 }
 node;
 
-int unload(node *list);
-void u(node *list);
+#define LIST_SIZE 2
 
-int main(int argc, char* argv)
+int unload(node *list);
+void visualizer(node *list);
+
+int main(void)
 {
-    node *list = '\0';
+    node *list = NULL;
+
+    // Add items to list
+    for (int i = 0; i < LIST_SIZE; i++)
+    {
+        char* phrase = get_string("Enter a new phrase: ");
+
+        // TODO: add phrase to new node in list
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return 1;
+        }
+
+        n->phrase = phrase;
+        // n->next = NULL;
+        n->next = list;
+        list = n;
+
+        // Visualize list after adding a node
+        visualizer(list);
+    }
+
+    if (unload(list) == 0)
+    {
+        printf("Error freeing the list.\n");
+        return 1;
+    }
+
+    printf("Freed the list.\n");
+    return 0;
+}
+
+int unload(node *list)
+{
+    // TODO: Free all allocated nodes
+    node *ptr = list;
+
+    while (ptr != NULL)
+    {
+        ptr = list->next;
+        free(list);
+        list = ptr;
+    }
+
+    return 1;
+}
+
+void visualize(node *list)
+{
     node *n = malloc(sizeof(node));
     n->phrase="Hi!";
     n->next='\0';
