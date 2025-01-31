5f8fbb9bfde234f407aaa4141fd3d57bc6464a08
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-31 01:54:06 -0300

    Java: update stdout formatting

--- java/DataTypes.java
+++ java/DataTypes.java
@@ -2,95 +2,73 @@ public class DataTypes {
     
     // Alphabetic DataTypes
     public static void charDataType() {
-        byte charBytes = Character.BYTES;
-        byte charSize = Character.SIZE;
-        byte charMin = Character.MIN_VALUE;
+        byte charBytes = Character.BYTES, charSize = Character.SIZE, charMin = Character.MIN_VALUE;
         int charMax = Character.MAX_VALUE;
         System.out.println("\n---------Char---------");
-        System.out.println("Byte bytes: " + charBytes);
-        System.out.println("Byte size: " + charSize);
-        System.out.println("Min byte value: " + charMin);
-        System.out.println("Max byte value: " + charMax);
+        System.out.println("Bytes: " + charBytes);
+        System.out.println("Size: " + charSize);
+        System.out.println("Range value: " + charMin + " to " + charMax + ")");
         System.out.println("----------------------");
     }
     
     // Numeric Data Types
     public static void byteDataType() {
-        byte byteBytes = Byte.BYTES;
-        byte byteSize = Byte.SIZE;
-        byte byteMin = Byte.MIN_VALUE;
-        byte byteMax = Byte.MAX_VALUE;
+        byte byteBytes = Byte.BYTES, byteSize = Byte.SIZE, byteMin = Byte.MIN_VALUE, byteMax = Byte.MAX_VALUE;
         System.out.println("\n---------Byte---------");
-        System.out.println("Byte bytes: " + byteBytes);
-        System.out.println("Byte size: " + byteSize);
-        System.out.println("Min byte value: " + byteMin);
-        System.out.println("Max byte value: " + byteMax);
+        System.out.println("Bytes: " + byteBytes);
+        System.out.println("Size: " + byteSize);
+        System.out.println("Range value: (" + byteMin + " to " + byteMax + ")");
         System.out.println("----------------------");
     }
 
     public static void shortDataType() {
-        byte shortBytes = Short.BYTES;
-        byte shortSize = Short.SIZE;
-        short shortMin = Short.MIN_VALUE;
-        short shortMax = Short.MAX_VALUE;
+        byte shortBytes = Short.BYTES, shortSize = Short.SIZE;
+        short shortMin = Short.MIN_VALUE, shortMax = Short.MAX_VALUE;
         System.out.println("\n---------Short--------");
-        System.out.println("Short bytes: " + shortBytes);
-        System.out.println("Short size: " + shortSize);
-        System.out.println("Min short value: " + shortMin);
-        System.out.println("Max short value: " + shortMax);
+        System.out.println("Bytes: " + shortBytes);
+        System.out.println("Size: " + shortSize);
+        System.out.println("Range value: (" + shortMin + " to " + shortMax + ")");
         System.out.println("----------------------");
     }
 
     public static void intDataType() {
-        byte intBytes = Integer.BYTES;
-        byte intSize = Integer.SIZE;
-        int intMin = Integer.MIN_VALUE;
-        int intMax = Integer.MAX_VALUE;
+        byte intBytes = Integer.BYTES, intSize = Integer.SIZE;
+        int intMin = Integer.MIN_VALUE, intMax = Integer.MAX_VALUE;
         System.out.println("\n--------Integer-------");
-        System.out.println("Integer bytes: " + intBytes);
-        System.out.println("Integer size: " + intSize);
-        System.out.println("Min integer value: " + intMin);
-        System.out.println("Max integer value: " + intMax);
+        System.out.println("Bytes: " + intBytes);
+        System.out.println("Size: " + intSize);
+        System.out.println("Range value: (" + intMin + " to " + intMax + ")");
         System.out.println("----------------------");
     }
 
     public static void longDataType() {
-        byte longBytes = Long.BYTES;
-        byte longSize = Long.SIZE;
-        long longMin = Long.MIN_VALUE;
-        long longMax = Long.MAX_VALUE;
+        byte longBytes = Long.BYTES, longSize = Long.SIZE;
+        long longMin = Long.MIN_VALUE, longMax = Long.MAX_VALUE;
         System.out.println("\n---------Long---------");
-        System.out.println("Long bytes: " + longBytes);
-        System.out.println("Long size: " + longSize);
-        System.out.println("Min long value: " + longMin);
-        System.out.println("Max long value: " + longMax);
+        System.out.println("Bytes: " + longBytes);
+        System.out.println("Size: " + longSize);
+        System.out.println("Range value: (" + longMin + " to " + longMax + ")");
         System.out.println("----------------------");
     }
 
     // Floating Numeric DataTypes
     public static void floatDataType() {
-        byte floatBytes = Float.BYTES;
-        byte floatSize = Float.SIZE;
-        float floatMin = Float.MIN_VALUE;
-        float floatMax = Float.MAX_VALUE;
+        byte floatBytes = Float.BYTES, floatSize = Float.SIZE;
+        float floatMin = Float.MIN_VALUE, floatMax = Float.MAX_VALUE;
         System.out.println("\n---------Float--------");
-        System.out.println("Float bytes: " + floatBytes);
-        System.out.println("Float size: " + floatSize);
-        System.out.println("Float min value: " + floatMin);
-        System.out.println("Float max value: " + floatMax);
+        System.out.println("Bytes: " + floatBytes);
+        System.out.println("Size: " + floatSize);
+        System.out.println("Range value: (" + floatMin + " to " + floatMax + ")");
         System.out.println("----------------------");
     }
 
     public static void doubleDataType() {
-        byte doubleBytes = Double.BYTES;
-        byte doubleSize = Double.SIZE;
-        double doubleMin = Double.MIN_VALUE;
-        double doubleMax = Double.MAX_VALUE;
+        byte doubleBytes = Double.BYTES, doubleSize = Double.SIZE;
+        double doubleMin = Double.MIN_VALUE, doubleMax = Double.MAX_VALUE;
         System.out.println("\n---------Float--------");
-        System.out.println("Double bytes: " + doubleBytes);
-        System.out.println("Double size: " + doubleSize);
-        System.out.println("Double min value: " + doubleMin);
-        System.out.println("Double max value: " + doubleMax);
+        System.out.println("Bytes: " + doubleBytes);
+        System.out.println("Size: " + doubleSize);
+        System.out.println("Range value: (" + doubleMin + " to " + doubleMax + ")");
         System.out.println("----------------------");
     }
 }
