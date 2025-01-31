0b7bf20a05b47a8b6fc971eba44018b6076e1737
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-31 00:34:35 -0300

    Java: see integer properties

--- java/main.java
+++ java/main.java
@@ -1,5 +1,17 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        intDataType();
+    }
+
+    public static void intDataType() {
+        int intMin = Integer.MIN_VALUE;
+        int intMax = Integer.MAX_VALUE;
+        int intSize = Integer.SIZE;
+        int intByte = Integer.BYTES;
+        System.out.println("Min Integer number: " + intMin);
+        System.out.println("Max Integer number: " + intMax);
+        System.out.println("Integer Size: " + intSize);
+        System.out.println("Integer Bytes: " + intByte);
     }
 }
