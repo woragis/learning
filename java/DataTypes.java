8f7fec0565db485256837f0f25e454a65f9325b1
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-31 00:52:08 -0300

    Java: add char type and improve memory usage in variables

--- java/DataTypes.java
+++ java/DataTypes.java
@@ -1,46 +1,68 @@
 public class DataTypes {
+    
+    // Alphabetic DataTypes
+    public static void charDataType() {
+        byte charBytes = Character.BYTES;
+        byte charSize = Character.SIZE;
+        byte charMin = Character.MIN_VALUE;
+        int charMax = Character.MAX_VALUE;
+        System.out.println("---------Char---------");
+        System.out.println("Byte Bytes: " + charBytes);
+        System.out.println("Byte Size: " + charSize);
+        System.out.println("Min Byte number: " + charMin);
+        System.out.println("Max Byte number: " + charMax);
+        System.out.println("----------------------");
+    }
+    
     // Numeric Data Types
     public static void byteDataType() {
+        byte byteBytes = Byte.BYTES;
+        byte byteSize = Byte.SIZE;
         byte byteMin = Byte.MIN_VALUE;
         byte byteMax = Byte.MAX_VALUE;
-        byte byteSize = Byte.SIZE;
-        byte byteByte = Byte.BYTES;
+        System.out.println("---------Byte---------");
+        System.out.println("Byte Bytes: " + byteBytes);
+        System.out.println("Byte Size: " + byteSize);
         System.out.println("Min Byte number: " + byteMin);
         System.out.println("Max Byte number: " + byteMax);
-        System.out.println("Byte Size: " + byteSize);
-        System.out.println("Byte Bytes: " + byteByte);
+        System.out.println("----------------------");
     }
 
     public static void shortDataType() {
+        byte shortBytes = Short.BYTES;
+        byte shortSize = Short.SIZE;
         short shortMin = Short.MIN_VALUE;
         short shortMax = Short.MAX_VALUE;
-        short shortSize = Short.SIZE;
-        short shortByte = Short.BYTES;
+        System.out.println("---------Short--------");
+        System.out.println("Short Bytes: " + shortBytes);
+        System.out.println("Short Size: " + shortSize);
         System.out.println("Min Short number: " + shortMin);
         System.out.println("Max Short number: " + shortMax);
-        System.out.println("Short Size: " + shortSize);
-        System.out.println("Short Bytes: " + shortByte);
     }
 
     public static void intDataType() {
+        byte intBytes = Integer.BYTES;
+        byte intSize = Integer.SIZE;
         int intMin = Integer.MIN_VALUE;
         int intMax = Integer.MAX_VALUE;
-        int intSize = Integer.SIZE;
-        int intByte = Integer.BYTES;
+        System.out.println("--------Integer-------");
+        System.out.println("Integer Bytes: " + intBytes);
+        System.out.println("Integer Size: " + intSize);
         System.out.println("Min Integer number: " + intMin);
         System.out.println("Max Integer number: " + intMax);
-        System.out.println("Integer Size: " + intSize);
-        System.out.println("Integer Bytes: " + intByte);
+        System.out.println("----------------------");
     }
 
     public static void longDataType() {
+        byte longBytes = Long.BYTES;
+        byte longSize = Long.SIZE;
         long longMin = Long.MIN_VALUE;
         long longMax = Long.MAX_VALUE;
-        long longSize = Long.SIZE;
-        long longByte = Long.BYTES;
+        System.out.println("---------Long---------");
+        System.out.println("Long Bytes: " + longBytes);
+        System.out.println("Long Size: " + longSize);
         System.out.println("Min Long number: " + longMin);
         System.out.println("Max Long number: " + longMax);
-        System.out.println("Long Size: " + longSize);
-        System.out.println("Long Bytes: " + longByte);
+        System.out.println("----------------------");
     }
 }
--- java/main.java
+++ java/main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!\n");
         System.out.println("Data types properties:");
+        DataTypes.charDataType();
         DataTypes.byteDataType();
         DataTypes.shortDataType();
         DataTypes.intDataType();
