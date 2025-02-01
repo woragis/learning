bf663b10ae103cb8dcd913f27479c5099dd2f8bb
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-01 20:56:33 -0300

    Edx: CS50: initialize stack and queue data structures

---
+++ edx/cs50/week-5-data-structures/queues.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _queue
+{
+    int arr[10];
+    int first;
+}
+queue;
+
+int main(void)
+{
+    queue myQueue;
+}
+
+void add(queue* q, int data) {}
+
+void pop(queue* q) {}
---
+++ edx/cs50/week-5-data-structures/stack.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _stack
+{
+    int arr[10];
+    int top;
+}
+stack;
+
+int main(void)
+{
+    stack myStack;
+}
+
+void push(stack* s, int data) {}
+
+void pop(stack* s) {}
