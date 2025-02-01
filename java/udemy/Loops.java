7ee3d78e2569d5f8729c73a4b1c743a3d20c63e1
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-01 01:25:34 -0300

    Java udemy: learn about loops java

---
+++ java/udemy/Loops.java
@@ -0,0 +1,8 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int counter = 0; counter < 100; counter++) {
+            System.out.print(counter + " ");
+        }
+        System.out.println("Hello, World!");
+    }
+}
