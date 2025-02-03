ada2b1b02dd331ba37d77d9a64b1a04084db77ce
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-03 01:52:33 -0300

    Edx: CS50: create stack data structure

--- edx/cs50/week-5-data-structures/stack.c
+++ edx/cs50/week-5-data-structures/stack.c
@@ -11,6 +11,13 @@ stack;
 int main(void)
 {
     stack myStack;
+    push(&myStack, 4);
+    push(&myStack, 5);
+    push(&myStack, 8);
+    push(&myStack, 12);
+    push(&myStack, 20);
+    push(&myStack, 21);
+    pop(&myStack);
 }
 
 void push(stack* s, int data) {}
