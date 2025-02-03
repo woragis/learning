4fda9d2a7cefe53efd38e337c1ec81cbd05b0a29
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-03 01:53:12 -0300

    Edx: CS50: initialize hash table data structure

---
+++ edx/cs50/week-5-data-structures/hash_tables.c
@@ -0,0 +1,29 @@
+/*
+ ****************
+ ** HASH TABLE **
+ ****************
+ * Create a HASH FUNCTION
+ *
+ * The HASH FUNCTION maps the data to the index
+ *
+ * The index will have a TABLE
+ *
+ * The table will store a pointer to a LINKED LIST
+ *
+ * The LINKED LIST will store items that cluster in the same index
+ *
+ * Resolving the problem of collision
+ *
+ * This is called
+ **************
+ ** CHAINING **
+ **************
+ *
+ * This Solution even gives me the idea of using a BINARY TREE inside the HASH TABLE
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{}
