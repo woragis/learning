f0c2a7664f9293572975ea2fb0ec7fad34e3c855
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-04 22:22:40 -0300

    Edx: CS50 Python: improve code quality

--- edx/cs50-python/week-6-file-io/names.py
+++ edx/cs50-python/week-6-file-io/names.py
@@ -1,9 +1,28 @@
+from typing import List
+
+
 def main():
-    file = open('names.txt', 'a')
-    for _ in range(3):
-        name = input('What\'s your name? ')
-        file.write(f'{name}\n')
-    file.close()
+    names: List[str] = []
+    with open('names.txt', 'r') as file:
+        for line in file:
+            names.append(line.rstrip())
+    for name in sorted(names):
+        print(f'Hello, {name.title()}')
+
+
+def read_file(file_path: str) -> List[str]:
+    lines: List[str] = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            lines.append(line)
+    return lines
+
+
+def append_file(file_path: str, inputs: List[str]) -> None:
+    with open(file_path, 'a') as file:
+        for input in inputs:
+            file.write(input)
+    return
 
 
 if __name__ == '__main__':
