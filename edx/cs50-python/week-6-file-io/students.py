545fa0b09bf6fd3b3aa8a0856c0974b46dccd6a1
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-04 23:11:59 -0300

    Edx: CS50 Python: learn how to use csv package for csv file handling

--- edx/cs50-python/week-6-file-io/students.py
+++ edx/cs50-python/week-6-file-io/students.py
@@ -1,35 +1,15 @@
+import csv
 from typing import Dict, List
-from names import read_file, append_file
 
-students_inputs: List[str] = []
-STUDENTS_COUNT = 5
 FILE_NAME = 'students.csv'
-
-
-def add_student():
-    for _ in range(STUDENTS_COUNT):
-        character = ''
-        name = input('What\'s the name of your character? ')
-        house = input('What\'s the house of your character? ')
-        character = ','.join([name, house])
-        students_inputs.append(character)
-
-    append_file(FILE_NAME, students_inputs)
-
-
-my_file = read_file(FILE_NAME)
-
 students: List[Dict[str, str]] = []
-for line in my_file:
-    name, house = line.split(',')
-    student: Dict[str, str] = {'name': name, 'house': house}
-    students.append(student)
 
-
-def get_name(student: Dict[str, str]):
-    return student['name']
+with open(FILE_NAME, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        student = {'name': row[0], 'house': row[1]}
+        students.append(student)
 
 
 for student in sorted(students, key=lambda student: student['name']):
-    # Transform CSV string into a 2 dimensional row
     print(f'{student['name']} is in {student['house']}')
