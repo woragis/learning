9355a478c4e6d9da6b951509758b8df15e8bc11f
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-04 23:20:01 -0300

    Edx: CS50 Python: learn about csv DictWriter and DictReader

--- edx/cs50-python/week-6-file-io/students.py
+++ edx/cs50-python/week-6-file-io/students.py
@@ -4,12 +4,25 @@ from typing import Dict, List
 FILE_NAME = 'students.csv'
 students: List[Dict[str, str]] = []
 
-with open(FILE_NAME, 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        student = {'name': row[0], 'house': row[1]}
-        students.append(student)
+
+def add_student():
+    with open(FILE_NAME, 'a') as file:
+        writer = csv.DictWriter(file, fieldnames=['name', 'house'])
+        new_student: Dict[str, str] = {}
+        new_student['name'] = input('What\'s your name? ')
+        new_student['house'] = input('What\'s your house? ')
+        writer.writerow(new_student)
 
 
-for student in sorted(students, key=lambda student: student['name']):
-    print(f'{student['name']} is in {student['house']}')
+def read_students():
+    with open(FILE_NAME, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            student = {'name': row['name'], 'house': row['house']}
+            students.append(student)
+
+    for student in sorted(students, key=lambda student: student['name']):
+        print(f'{student['name']} is in {student['house']}')
+
+
+read_students()
