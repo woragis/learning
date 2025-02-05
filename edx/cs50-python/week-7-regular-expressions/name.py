acb7b2b3417a912035c5e6ef2f6d4c07ae250533
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-05 09:37:55 -0300

    Edx: CS50 Python: create a name validator with regex

---
+++ edx/cs50-python/week-7-regular-expressions/name.py
@@ -0,0 +1,7 @@
+import re
+
+name = input('What\'s your name? ').strip()
+if matches := re.search(r'^(.+), *(.+)$', name):
+    name = matches.group(2) + ' ' + matches.group(1)
+
+print(f'Hello, {name}')
