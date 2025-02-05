b2eea21e6b32abf37dc002c3d662dc286643cddd
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-05 10:39:40 -0300

    Edx: CS50 Python: add patronus property to student class

--- edx/cs50-python/week-8-oop/student.py
+++ edx/cs50-python/week-8-oop/student.py
@@ -1,18 +1,39 @@
+class Student:
+    houses = ('slytherin', 'gryffidor', 'ravenclaw', 'hufflepuff')
+
+    def __init__(self, name: str, house: str, patronus: str) -> None:
+        if not name:
+            raise ValueError('Missing name')
+        if house not in self.houses:
+            raise ValueError('Invalid house')
+
+        self.name: str = name
+        self.house = house
+        self.patronus = patronus
+
+    def __str__(self) -> str:
+        return f'{self.name} is from {self.house} and has {self.patronus} as patronus'
+
+    def charm(self):
+        match self.patronus:
+            case 'Stag': return 'horse'
+            case 'Otter': return 'lagosta'
+            case 'Jack Russell Terrier': return 'dog'
+            case _: return 'pencil'
+
+
 def main():
     student = get_student()
-    if student['name'] == 'Padma':
-        student['house'] = 'Ravenclaw'
-    print(f'{student['name']} is from {student['house']}')
+    print('Expectro Patronum!')
+    print(student.charm())
+    print(student)
 
 
 def get_student():
-    name = get_name()
-    house = get_house()
-    return {'name': name, 'house': house}
-
-
-def get_name(): return input('Name: ').capitalize()
-def get_house(): return input('House: ').capitalize()
+    name = input('Name: ')
+    house = input('House: ')
+    patronus = input('Patronus: ')
+    return Student(name, house, patronus)
 
 
 if __name__ == '__main__':
