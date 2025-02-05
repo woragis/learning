5de518c2c405d2010ccfa5883579638068ae2ea8
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-04 22:13:42 -0300

    Edx: CS50 Python: update loop and file handling formatting

--- edx/cs50-python/week-6-file-io/names.py
+++ edx/cs50-python/week-6-file-io/names.py
@@ -1,9 +1,9 @@
 def main():
+    file = open('names.txt', 'a')
     for _ in range(3):
         name = input('What\'s your name? ')
-        file = open('names.txt', 'a')
         file.write(f'{name}\n')
-        file.close()
+    file.close()
 
 
 if __name__ == '__main__':
