e118a60f1516a9aae2dde16241e2fd0e37b7e0bf
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-04 22:56:23 -0300

    Edx: CS50 Python: display students sorted by first character

--- edx/cs50-python/week-6-file-io/students.py
+++ edx/cs50-python/week-6-file-io/students.py
@@ -1,19 +1,29 @@
 from typing import List
 from names import read_file, append_file
 
-inputs: List[str] = []
+students_inputs: List[str] = []
 STUDENTS_COUNT = 5
 FILE_NAME = 'students.csv'
 
-for _ in range(STUDENTS_COUNT):
-    character = ''
-    name = input('What\'s the name of your character? ')
-    house = input('What\'s the house of your character? ')
-    character = ','.join([name, house])
-    inputs.append(character)
 
-append_file(FILE_NAME, inputs)
+def add_student():
+    for _ in range(STUDENTS_COUNT):
+        character = ''
+        name = input('What\'s the name of your character? ')
+        house = input('What\'s the house of your character? ')
+        character = ','.join([name, house])
+        students_inputs.append(character)
+
+    append_file(FILE_NAME, students_inputs)
+
 
 my_file = read_file(FILE_NAME)
+
+students: List[str] = []
 for line in my_file:
-    print(line)
+    students.append(line)
+
+for student in sorted(students):
+    # Transform CSV string into a 2 dimensional row
+    name, house = student.split(',')
+    print(f'{name} is in {house}')
