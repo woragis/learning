b3b48b3c4496d929b4c9f0fbca6f4e36ac666ea2
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-06 11:09:52 -0300

    Edx: CS50 Python: create unpredicted arguments friendly function

--- edx/cs50-python/week-9-et-cetera/unpack.py
+++ edx/cs50-python/week-9-et-cetera/unpack.py
@@ -7,6 +7,21 @@ def main():
     dict_total = total(**coins_dict)
     print(f'Total: {dict_total} knuts')
 
+    print()
+    f(100, 50, 25, 5)
+    f(*coins_list)
+    print()
+
+    f(galleons=100, sickles=50, knuts=25, pennies=5)
+    f(**coins_dict)
+
+
+def f(*args: int, **kwargs: int):
+    if args:
+        print(f'Positional: {args}')
+    if kwargs:
+        print(f'Named: {kwargs}')
+
 
 def total(galleons: int, sickles: int, knuts: int):
     return (galleons*17 + sickles)*29 + knuts
