de5733051e7d3ecbcceab29b77d94b1c28172b77
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-06 11:29:35 -0300

    Edx: CS50 Python: use map str method in yell function

--- edx/cs50-python/week-9-et-cetera/yell.py
+++ edx/cs50-python/week-9-et-cetera/yell.py
@@ -1,16 +1,9 @@
-from typing import List
-
-
 def main():
     yell('This', 'is', 'CS50')
 
 
 def yell(*words: str):
-    uppercased: List[str] = []
-
-    for word in words:
-        uppercased.append(word.upper())
-
+    uppercased: map[str] = map(str.upper, words)
     print(*uppercased)
 
 
