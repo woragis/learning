3762b5e1ffbcaad159bb7ce063dd5372d85d348a
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-06 10:55:50 -0300

    Edx: CS50 Python: create command line program

--- edx/cs50-python/week-9-et-cetera/meows.py
+++ edx/cs50-python/week-9-et-cetera/meows.py
@@ -1,11 +1,19 @@
-class Cat:
-    MEOWS = 3
+import argparse
 
-    def meow(self):
-        for _ in range(self.MEOWS):
-            print('meow')
+
+def main():
+    parser = argparse.ArgumentParser(description='Meow like a cat')
+    parser.add_argument(
+        '-n', default=1, help='Number of times to meow', type=int)
+    args = parser.parse_args()
+
+    meow(args.n)
+
+
+def meow(n: int):
+    for _ in range(n):
+        print('meow')
 
 
 if __name__ == '__main__':
-    cat = Cat()
-    cat.meow()
+    main()
