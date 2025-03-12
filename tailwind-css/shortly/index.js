fb735da427a06a4ae3e721b32c7b20d6f1b58438
woragis <masteringthecode.woragis+github@gmail.com>
2025-03-12 11:21:45 -0300

    TailwindCSS: Shortly: Add hamburger menu button logic and styling

--- tailwind-css/shortly/index.js
+++ tailwind-css/shortly/index.js
@@ -1,6 +1,15 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
 const input = document.getElementById('link-input')
 const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
+
+btn.addEventListener('click', function () {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+})
 
 function validURL(str) {
   var pattern = new RegExp(
