2488ddbf880f318866892f65e25b2ea5b3e36e1f
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-29 13:45:51 -0300

    Asm: Learn about x64 asm registers

---
+++ asm/registers.asm
@@ -0,0 +1,49 @@
+/*
+
+===========================
+Registers
+===========================
+
+rax - syscall number
+rcx - counter
+rdx - length of data
+rbx - length of input string
+rsp - stack pointer
+rdi - file descriptor
+rsi - pointer to buffer
+r8  - pointer to key
+r9  - pointer to result
+r10 - pointer to string
+r11 - pointer to string
+r12 - pointer to string
+r13 - pointer to string
+r14 - pointer to string
+r15 - pointer to string
+
+===========================
+General Purpose Registers:
+===========================
+
+
+r0 -> rax
+r1 -> rcx
+r2 -> rdx
+r3 -> rbx
+r4 -> rsi
+r5 -> rbp
+r6 -> rsi
+r7 -> rdi
+r8 -> r8
+r9 -> r9
+r10 -> r10
+r11 -> r11
+r12 -> r12
+r13 -> r13
+r14 -> r14
+r15 -> r15
+
+Stack Pointer (rsp) - points to the top of the stack.
+Base Pointer (rbp) - points to the base of the stack frame.
+Instruction Pointer (rip) - points to the next instruction to
+
+*/
