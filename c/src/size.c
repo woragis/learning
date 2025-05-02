94aee3308295bd64d0fd38413545e3b91c4aeaec
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-02 12:28:29 -0300

    C: Add the sizeof 'short'

--- c/src/size.c
+++ c/src/size.c
@@ -5,6 +5,7 @@ int main()
 {
     printf("Size of 'char': %d\n", sizeof(char));
     printf("Size of 'bool': %d\n", sizeof(bool));
+    printf("Size of 'short': %d\n", sizeof(short));
     printf("Size of 'int': %d\n", sizeof(int));
     printf("Size of 'long': %d\n", sizeof(long));
     printf("Size of 'long long': %d\n", sizeof(long long));
