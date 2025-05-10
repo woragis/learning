a9710463d7fee337a9e2b350e1297d80e6d8ae33
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-09 23:37:49 -0300

    C++: Learn about c++ primitive data types

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -14,3 +14,22 @@ int main()
 
     return 0;
 }
+
+void data_types()
+{
+    // unsigned short
+    unsigned short int exam_score{55};
+    cout << "My exam score was " << exam_score << endl;
+
+    // signed int
+    int countries_represented{65};
+    cout << "There were " << countries_represented << " countries represented in my meeting!" << endl;
+
+    // signed long int
+    long people_in_florida{20610000};
+    cout << "There are about " << people_in_florida << " people in Florida!" << endl;
+
+    // signed long long int
+    long long people_on_earth{8'000'000'000};
+    cout << "There are about " << people_on_earth << " people on earth!" << endl;
+}
