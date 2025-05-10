84638a6eac494094c29785d13f2b56dcf8340291
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-10 16:25:32 -0300

    C++: Separate functions

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -4,15 +4,24 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+void hello_world();
+void data_types();
+
 int main()
+{
+    hello_world();
+    data_types();
+
+    return 0;
+}
+
+void hello_world()
 {
     cout << "Hello, World!" << endl;
     cout << "What\'s your name? " << endl;
     char name[50];
     cin >> name;
     cout << "Your name is: " << name << "?" << endl;
-
-    return 0;
 }
 
 void data_types()
