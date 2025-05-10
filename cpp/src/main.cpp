e49c42305b6f8b4095a67cd60d5af8af5e722478
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-10 16:23:56 -0300

    C++: Add more c++ data types

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -17,6 +17,9 @@ int main()
 
 void data_types()
 {
+    /***************************
+     *   Integer types
+     ***************************/
     // unsigned short
     unsigned short int exam_score{55};
     cout << "My exam score was " << exam_score << endl;
@@ -32,4 +35,30 @@ void data_types()
     // signed long long int
     long long people_on_earth{8'000'000'000};
     cout << "There are about " << people_on_earth << " people on earth!" << endl;
+
+    /***************************
+     *   Floating point types
+     ***************************/
+    float car_payment{401.23};
+    cout << "My car payment is " << car_payment << endl;
+
+    double pi{3.14159};
+    cout << "Pi is" << pi << endl;
+
+    long double large_amount{2.7e120};
+    cout << large_amount << "is a very big number!" << endl;
+
+    /***************************
+     *   Boolean types
+     ***************************/
+    bool gameOver{false};
+    cout << "The value of gameOver is " << gameOver << endl;
+
+    /***************************
+     *   Overflow example
+     ***************************/
+    short value1{30000};
+    short value2{1000};
+    short sum{value1 * value2};
+    cout << "The sum of " << value1 << " and " << value2 << " is " << sum << endl;
 }
