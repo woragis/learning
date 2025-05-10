dfbd2a638308eba8d8289d9362f5f493b1581a8f
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-09 23:12:04 -0300

    C++: Learn about namespace

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+using std::cin;
+using std::cout;
+using std::endl;
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "What\'s your name? " << endl;
+    char name[50];
+    cin >> name;
+    cout << "Your name is: " << name << "?" << endl;
 
     return 0;
 }
