8610fd7f3332a102858e4bfb94cb1d990ecadae2
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-04 01:22:45 -0300

    Lua: Learn about variables in lua

---
+++ lua/variables.lua
@@ -0,0 +1,14 @@
+local sum = 1+1
+print(sum)
+
+sum = 'Hello guys'
+print(sum)
+
+sum = 'seems like this variable doesnt have a fixed type'
+print(sum)
+
+sum = false
+print(sum)
+sum = true
+print(sum)
+
